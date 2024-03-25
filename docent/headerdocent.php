d7d4d26d1973ca10d5624e13f12347f4d7bacc3a
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2024-03-25 14:55:52 +0100

    split header, dashboard and footer for docentpanel

---
+++ docent/headerdocent.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docent Dashboard</title>
+    <link rel="stylesheet" href="../styles/docent.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container-fluid">
+        <img src="../assets/logo.png" alt="logo" class="logo">
+        <nav>
+            <div class="nav nav-underline">
+            <ul class="nav nav-underline">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">My Account</a>
+                </li>
+                <li class="nav-item">
+                    <button><a class="nav-link" href="index.php">Logout</a></button>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <hr>
+    <div class="container">
