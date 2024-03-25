49cc5e86b6600f0a6ae0ec1429baeb6059985040
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2024-03-25 14:43:49 +0100

    split into header, dashboard and footer & using include

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -1,33 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stage2024</title>
-    <link rel="stylesheet" href="styles/admin.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container-fluid">
-        <img src="assets/logo.png" alt="logo" class="logo">
-        <nav>
-            <div class="nav nav-underline">
-            <ul class="nav nav-underline">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">My Account</a>
-                </li>
-                <li class="nav-item">
-                    <button><a class="nav-link" href="index.php">Logout</a></button>
-                </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-    <hr>
-    <div class="container">
+<?php include('header.php'); ?>
+
         <div class="row">
             <div class="col-md text-center">
                 <h1>Welkom op het SyntraPXL ADMIN portaal</h1>
@@ -60,7 +32,5 @@
         </div>
     </div>
 
-
+<?php include('footer.php'); ?>
     
-</body>
-</html>
