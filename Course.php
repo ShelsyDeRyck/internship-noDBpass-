8ac9668d7735dd0399f23f15609be210a5b8c9d9
beau pirard <beau.pirard@hotmail.com>
2024-03-26 10:38:19 +0100

    Added a return to admin panel button.

--- Course.php
+++ Course.php
@@ -64,6 +64,8 @@ $courses = $db->query("SELECT * FROM courses")->fetchAll(PDO::FETCH_ASSOC);
     <link rel="stylesheet" href="styles/course.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+<div class="container">
+<a href="admin_dashboard.php" class="btn btn-orange">Terug naar admin panel</a>
 <body>
 
 <?php
