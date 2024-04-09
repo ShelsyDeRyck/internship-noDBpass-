926ae0fe81892edadf36035a22507947344cbb94
Adam Gazdiev <a.gazdiev@gmail.com>
2024-04-09 10:34:44 +0200

    Changed the Update method of the Intership class

--- Internship.php
+++ Internship.php
@@ -53,18 +53,39 @@ class Internship {
     
     
 
-    // Updating internship information
-    public function update($internship_id, $company_id, $address, $contact_person_id, $start_date, $end_date, $student_id) {
-        try {
-            $sql = "UPDATE internships SET company_id = ?, address = ?, contact_person_id = ?, start_date = ?, end_date = ?, student_id = ? WHERE id = ?";
-            $stmt = $this->db->prepare($sql);
-            $stmt->execute([$company_id, $address, $contact_person_id, $start_date, $end_date, $student_id, $internship_id]);
-            return true;
-        } catch (PDOException $e) {
-            // Ideally, log the error
-            return false;
-        }
+    // Updating information about an internship
+public function update($internshipData) {
+    try {
+        // Begin transaction
+        $this->db->beginTransaction();
+
+        // Update companies table
+        $sql = "UPDATE companies SET name = ?, address = ? WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$internshipData['company_name'], $internshipData['company_address'], $internshipData['company_id']]);
+
+        // Update contact_person table
+        $sql = "UPDATE contact_person SET first_name = ?, last_name = ?, email = ?, phone = ? WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$internshipData['contact_first_name'], $internshipData['contact_last_name'], $internshipData['contact_email'], $internshipData['contact_phone'], $internshipData['contact_person_id']]);
+
+        // Update internships table
+        $sql = "UPDATE internships SET student_id = ?, start_date = ?, end_date = ? WHERE id = ?";
+        $stmt = $this->db->prepare("UPDATE internships SET student_id = ? WHERE id = ?");
+        $stmt->execute([$internshipData['student_id'], $internshipData['internship_id']]);
+
+
+        // Commit transaction
+        $this->db->commit();
+        return true;
+    } catch (PDOException $e) {
+        // Rollback transaction on error
+        $this->db->rollBack();
+        // Error handling should be implemented here
+        return false;
     }
+}
+
 
     // Deleting internship
     public function delete($internship_id) {
