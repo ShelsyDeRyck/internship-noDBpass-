8751ba6f2faa006b87e3a76427f4b763a57d48f5
ShelsyDeRyck <114619980+ShelsyDeRyck@users.noreply.github.com>
2024-04-10 16:00:30 +0200

    updated export mysql query, made most of the form

--- students/export_student.php
+++ students/export_student.php
@@ -4,7 +4,7 @@ ob_end_clean();
 // Databaseverbinding maken
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $dbname = "educational_center";
 
 session_start();
@@ -19,100 +19,86 @@ if(session_status() === PHP_SESSION_ACTIVE && isset($_SESSION['id']) && !empty($
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    //column names -> students (table)
-    //id, first_name, last_name, email, course_id, date_of_birth, study_year
-    // $student_columns = array('id', 'first_name', 'last_name', 'email', 'course_id', 'date_of_birth', 'study_year');
-
-    //student_course (table)
-    //course_id, student_id
-    // $student_course_columns = array('course_id', 'student_id');
-
-    //student grades (table)
-    //student_id, course_id, grade_id, teacher_id, grade, academic_year
-    // $student_grades_columns = array('student_id', 'course_id', 'grade_id', 'teacher_id', 'grade', 'academic_year');
-
-    //courses (table)
-    //course_id, name, description
-    // $courses_columns = array('course_id', 'name', 'description');
-
-    //internship (table)
-    //id, student_id, company_id, address, contact_person_id, start_date, end_date
-    // $internship_columns = array('id', 'student_id', 'company_id', 'address', 'contact_person_id', 'start_date', 'end_date');
-
-    //course (table)
-    //course_id, name, description, duration, location
-    // $course_columns = array('course_id', 'name', 'description', 'duration', 'location');
-  
+    
     $pdf = new \Dompdf\Dompdf(array('enable_remote'  =>  true));
 
     $query = $conn->prepare("
     SELECT 
-        s.id AS student_id,
-        s.first_name,
-        s.last_name,
-        s.email,
-        s.date_of_birth,
-        s.study_year,
-        c.course_id AS student_course_id,
-        c.name AS course_name,
-        c.description AS course_description,
-        sg.grade_id AS student_grade_id,
-        sg.teacher_id,
-        sg.grade,
-        sg.academic_year,
-        i.id AS internship_id,
-        i.company_id,
-        i.address AS internship_address,
-        i.contact_person_id AS internship_contact_person_id,
+        cp.first_name AS contact_person_first_name,
+        cp.last_name AS contact_person_last_name,
+        cp.email AS contact_person_email,
+        cp.phone AS contact_person_phone,
+        t.first_name AS teacher_first_name,
+        t.last_name AS teacher_last_name,
         i.start_date AS internship_start_date,
-        i.end_date AS internship_end_date
+        i.end_date AS internship_end_date,
+        c.name AS company_name
     FROM 
-        students AS s
+        students s
     LEFT JOIN 
-        student_course AS sc ON s.id = sc.student_id
+        internships i ON s.id = i.student_id
     LEFT JOIN 
-        courses AS c ON sc.course_id = c.course_id
+        contact_person cp ON i.contact_person_id = cp.id
     LEFT JOIN 
-        student_grades AS sg ON s.id = sg.student_id AND sc.course_id = sg.course_id
+        course_teacher ct ON s.course_id = ct.course_id
     LEFT JOIN 
-        internship AS i ON s.id = i.student_id
+        teachers t ON ct.teacher_id = t.id
+    LEFT JOIN
+        companies c ON i.company_id = c.id
     WHERE 
-        s.id = ?
+        s.id = ?; 
     ");
+
+    // sk.name AS skill_name,
+    // sk.type AS skill_type,
+    // sg.grade AS student_grade
+
+    // LEFT JOIN
+    //     skills sk ON ss.skill_id = sk.id
+    // LEFT JOIN
+    //     student_grades sg ON s.id = sg.student_id
+
     $query->bind_param('i', $id);
     $query->execute();
     $result = $query->get_result();
 
     if ($result->num_rows > 0) {
-        if ($result->num_rows > 0) {
-            $response = $result->fetch_assoc();
-            $student_id = $response['student_id'];
-            $first_name = $response['first_name'];
-            $last_name = $response['last_name'];
-            $email = $response['email'];
-            $date_of_birth = $response['date_of_birth'];
-            $study_year = $response['study_year'];
-            $student_course_id = $response['student_course_id'];
-            $course_name = $response['course_name'];
-            $course_description = $response['course_description'];
-            $student_grade_id = $response['student_grade_id'];
-            $teacher_id = $response['teacher_id'];
-            $grade = $response['grade'];
-            $academic_year = $response['academic_year'];
-            $internship_id = $response['internship_id'];
-            $company_id = $response['company_id'];
-            $internship_address = $response['internship_address'];
-            $internship_contact_person_id = $response['internship_contact_person_id'];
-            $internship_start_date = $response['internship_start_date'];
-            $internship_end_date = $response['internship_end_date'];
-        }
-    }
+        $response = $result->fetch_assoc();
+        $first_name = $response['contact_person_first_name'];
+        $last_name = $response['contact_person_last_name'];
+        $email = $response['contact_person_email'];
+        $phone = $response['contact_person_phone'];
+        $date = $response['internship_start_date'] . ' - ' . $response['internship_end_date'];
+        // print_r($response);
 
+        $html = "
+            <p><b>$first_name $last_name</b></p>
+            <p>$email</p>
+            <p>$phone</p>
 
-    if ($result->num_rows > 0) {
-        $html = "<h1>Student</h1>
-        <h2>Student ID: $id</h2>
-        <h3>Student name: $first_name $last_name</h3>";
+            <p><b>Docent</b></p>
+            <p>$response[teacher_first_name] $response[teacher_last_name]</p>
+
+            <p><b>Periode</b></p>
+            <p>$date</p>
+
+            <p><b>Omgeving: $response[company_name]</b></p>
+            <textarea></textarea>
+
+            <p><b>Scope</b></p>
+            <textarea></textarea>
+
+            <p><b>Evaluatie: (Hardskill/Softskill)</b></p>
+            
+            <p><b>Goede ervaringen en werkpunten: te bepalen door stagementor:</b></p>
+            <textarea></textarea>
+
+            <p><b>Zou u deze stagair op basis van uw ervaringen aannemen in uw bedrijf</b></p>
+            <textarea></textarea>
+
+            <p><b>Totaal:</b></p>
+            <p>Softskill: %, Hardskill: %</p>
+        ";
     
         $pdf->loadHtml($html);
         $pdf->setPaper('A4', 'portrait');
