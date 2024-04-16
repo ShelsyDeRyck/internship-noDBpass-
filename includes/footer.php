faa55c8af938dc3cc05c24ecb5a368b4d3b8d4f8
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2024-04-16 15:44:57 +0200

    new style admin dashboard + added hover design

--- includes/footer.php
+++ includes/footer.php
@@ -6,6 +6,7 @@
     height: 100px;
     padding: 30px;
     margin-top: auto;
+    border-top: 1px solid #EA302D;
 
     /* Add a subtle top shadow */
     box-shadow: 0 -5px 5px -5px rgba(0, 0, 0, 0.2); 
