d5caa73bbff83b7f693e26c597451068c8ce4e50
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2024-04-16 13:59:45 +0200

    finish design dashboard admin

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -11,32 +11,40 @@ if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'admins') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>    
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <?php include ('includes/bootstrap.php'); ?>
     <link rel="stylesheet" href="./styles/admin.css">
+    
 
-    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+    
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg mb-3">
     <div class="container-fluid">
         <ul class="navbar-nav firstnav">
             <a class="navbar-brand" href="../dashboard_admin.php">
                 <img src="assets/logo.png" alt="logo" width="100" height="50">
             </a>
         </ul>
-        <ul class="navbar-nav ms-auto secondnav">
-            <li class="nav-item">
-                <a class="nav-link" href="account/accountindex.php">Account</a>
-            </li>
-            <li class="nav-item">
-                    <button><a class="nav-link" href="logout.php">Logout</a></button>
-            </li>
-        </ul>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav ms-auto secondnav">
+                <li class="nav-item">
+                    <a class="nav-link" href="account/accountindex.php">Account</a>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="btn btn-sm btn-outline-danger"><a class="nav-link" href="logout.php">Logout</a></button>
+                </li>
+            </ul>
+        </div>
+        
     </div>
+    
     </nav>
-
-    <hr class="hr-nav">
 <div class="row">
     <div class="col-md text-center">
         <h1>Welkom op het SyntraPXL ADMIN portaal</h1>
